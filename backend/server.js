eef2670b1a6ea081b762522e632bd5671196d266
annanya17699 <annanya17699@gmail.com>
2022-02-01 17:17:00 +0530

    production backend

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,6 @@ app.use((error, req, res, next)=>{
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.naacr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 .then(()=>{
  console.log("connected to db");
- app.listen(5000);
+ app.listen(process.env.PORT || 5000);
 })
 .catch(err=> console.log(err));
